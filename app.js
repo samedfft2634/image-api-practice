b5871ef91cd8495655a6b86d448e26a4bc672cb2
samedfft2634 <samedfft@gmail.com>
2023-11-16 17:50:15 +0300

    random photos

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
 const searchBtn = document.getElementById("searchBtn"); //
 const searchInput = document.getElementById("searchInput");
-const accessKey = "aaj6YPiMnAhQba_1qtN0W-0vIg9jlgl0HZ6c43mW1ZM";
+const accessKey = "NOioFOJPL0nG_6dZ32V8MY7L5C8qCWiGKxigVpPnph4";
 
-
- 
+let searchCount = 0;
 
 searchBtn.addEventListener("click", async () => {
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm === "") {
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top",
-            showConfirmButton: false,
-            timer: 2000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.onmouseenter = Swal.stopTimer;
-              toast.onmouseleave = Swal.resumeTimer;
-            }
-          });
-          Toast.fire({
-            icon: "warning",
-            title: "There is no words to searching!"
-          });
-        setTimeout(()=>{
-            randomImage()
-        },200)
-    } else {
-        const API_URL = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${accessKey}`;
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        if (data.results) {
-            randomImage(data.results);
-        } else {
-            alert("An error occurred while importing images from Unsplash.");
-        }
-    }
+  if(searchCount === 30){
+    searchCount = 0;
+  } else {
+    searchCount = Math.floor(Math.random()*30) + 1
+  }
+	const searchTerm = searchInput.value.trim();
+	if (searchTerm === "") {
+		const Toast = Swal.mixin({
+			toast: true,
+			position: "top",
+			showConfirmButton: false,
+			timer: 2000,
+			timerProgressBar: true,
+			didOpen: (toast) => {
+				toast.onmouseenter = Swal.stopTimer;
+				toast.onmouseleave = Swal.resumeTimer;
+			},
+      
+		});
+		Toast.fire({
+			icon: "warning",
+			title: "There is no words to searching!",
+		});
+		setTimeout(() => {
+			randomImage();
+		}, 200);
+    return
+	} 
+
+  const API_URL = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${accessKey}&page=${searchCount}`;
+
+  try {
+		const response = await fetch(API_URL);
+		const data = await response.json();
+		if (data.results) {
+			randomImage(data.results);
+		} else {
+			alert("An error occurred while importing images from Unsplash.");
+		}
+	} catch (error) {
+    console.error("An error occurred during the API call:",error)
+  }
 });
 
 const randomImage = (images) => {
 	const cardMain = document.querySelector(".cardMain");
 	cardMain.innerHTML = ``;
+  // const shuffledImages = shuffleArray(images)
 	images.forEach((image) => {
 		const cardBody = `
         <div class="card col col-md-3 col-lg-6  " style="width: 20rem;">
@@ -51,3 +63,10 @@ const randomImage = (images) => {
 	});
 };
 
+// const shuffleArray = (array) => {
+//   for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//   }
+//   return array;
+// };
